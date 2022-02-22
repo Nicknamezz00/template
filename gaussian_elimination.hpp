f21f0608137d4e8258326f9045488c72eae5ec76
Nicknamezz00 <1315098969@qq.com>
2022-02-22 13:51:30 +0800

    Gaussian Elimination

--- gaussian_elimination.hpp
+++ gaussian_elimination.hpp
@@ -106,8 +106,3 @@ vector<T> SolveLinearSystem(vector<vector<T>> a, const vector<T>& b, int w) {
   }
   return x;
 }
-
-作者：AcWing
-链接：https://www.acwing.com/activity/content/public_group/record/226/
-来源：AcWing
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
