ae8f9b76c4b7270c38e476c246974eb542bb3c13
Nicknamezz00 <1315098969@qq.com>
2022-02-26 14:09:31 +0800

    fixed

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,28 +1,29 @@
 template <typename T>
-class digraph : public graph<T> {
- public:
-  using graph<T>::edges;
-  using graph<T>::g;
-  using graph<T>::n;
-  using graph<T>::ignore;
-  
-  digraph(int _n) : graph<T>(_n) {}
-
-  int add(int from, int to, T cost = 1) {
-    assert(0 <= from && from < n && 0 <= to && to < n);
-    int id = (int)edges.size();
-    g[from].push_back(id);
-    edges.push_back({from, to, cost});
-    return id;
-  }
-  digraph<T> reverse() const {
-    digraph<T> rev(n);
-    for (auto &e : edges) {
-      rev.add(e.to, e.from, e.cost);
+vector<T> dijkstra(const graph<T> &g, int start) {
+  assert(0 <= start && start < g.n);
+  vector<T> dist(g.n, numeric_limits<T>::max());
+  priority_queue<pair<T, int>, vector<pair<T, int> >, greater<pair<T, int> > > s;
+  dist[start] = 0;
+  s.emplace(dist[start], start);
+  while (!s.empty()) {
+    T expected = s.top().first;
+    int i = s.top().second;
+    s.pop();
+    if (dist[i] != expected) {
+      continue;
     }
-    if (ignore != nullptr) {
-      rev.set_ignore_edge_rule([&](int id) { return ignore(id); });
+    for (int id : g.g[i]) {
+      if (g.ignore != nullptr && g.ignore(id)) {
+        continue;
+      }
+      auto &e = g.edges[id];
+      int to = e.from ^ e.to ^ i;
+      if (dist[i] + e.cost < dist[to]) {
+        dist[to] = dist[i] + e.cost;
+        s.emplace(dist[to], to);
+      }
     }
-    return rev;  // returns numeric_limits<T>::max() if there's no path
   }
-};
+  return dist;
+  // returns numeric_limits<T>::max() if there's no path
+}
