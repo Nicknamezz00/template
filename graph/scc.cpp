66f2572540c1cf8318bc451b3fba2c8454943bbb
Nicknamezz00 <1315098969@qq.com>
2022-02-26 22:21:32 +0800

    change name

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -3,7 +3,7 @@ vector<int> find_scc(const digraph<T> &g, int &cnt) {
   digraph<T> g_rev = g.reverse();
   vector<int> order;
   vector<bool> was(g.n, false);
-  function<void(int)> tar = [&](int v) {
+  function<void(int)> dfs1 = [&](int v) {
     was[v] = true;
     for (int id : g.g[v]) {
       if (g.ignore != nullptr && g.ignore(id)) {
@@ -12,18 +12,18 @@ vector<int> find_scc(const digraph<T> &g, int &cnt) {
       auto &e = g.edges[id];
       int to = e.to;
       if (!was[to]) {
-        tar(to);
+        dfs1(to);
       }
     }
     order.push_back(v);
   };
   for (int i = 0; i < g.n; i++) {
     if (!was[i]) {
-      tar(i);
+      dfs1(i);
     }
   }
   vector<int> c(g.n, -1);
-  function<void(int)> jan = [&](int v) {
+  function<void(int)> dfs2 = [&](int v) {
     for (int id : g_rev.g[v]) {
       if (g_rev.ignore != nullptr && g_rev.ignore(id)) {
         continue;
@@ -32,18 +32,19 @@ vector<int> find_scc(const digraph<T> &g, int &cnt) {
       int to = e.to;
       if (c[to] == -1) {
         c[to] = c[v];
-        jan(to);
+        dfs2(to);
       }
     }
   };
   cnt = 0;
+  // Kosaraju 
   for (int id = g.n - 1; id >= 0; id--) {
     int i = order[id];
     if (c[i] != -1) {
       continue;
     }
     c[i] = cnt++;
-    jan(i);
+    dfs2(i);
   }
   return c;
   // c[i] <= c[j] for every edge i -> j
